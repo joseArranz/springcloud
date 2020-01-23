0a3cbea8ea770a479114372494d0ae49c835773f
DESKTOP-N62HNGM\Jose <jose.arranz.becerril.jab@gmail.com>
2020-01-23 16:06:16 +0100

    Mongo Connect

---
+++ src/main/java/com/jarranz/cloud/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.jarranz.cloud.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.jarranz.cloud.model.Customer;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+  public Customer findByFirstName(String firstName);
+  public List<Customer> findByLastName(String lastName);
+
+}
